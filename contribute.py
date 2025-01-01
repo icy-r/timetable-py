d442c6220e06d1e7e9f446af0226c1f6b7584351
Asath Mohamed <it22633422@my.sliit.lk>
2025-01-01 00:00:00 +0000

    Update: Jan 01 2025

--- contribute.py
+++ contribute.py
@@ -43,8 +43,8 @@ def commit_with_date(repo, date):
 def main():
     try:
         repo = Repo('.')
-        start_date = datetime(2024, 1, 1)
-        end_date = datetime(2024, 4, 15)
+        start_date = datetime(2025, 1, 1)
+        end_date = datetime(2025, 2, 14)
         current_date = start_date
 
         while current_date <= end_date:
