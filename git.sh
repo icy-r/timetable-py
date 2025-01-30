ecfdd370c14fce239aba431d226b6cc5a0163840
Asath Mohamed <it22633422@my.sliit.lk>
2025-01-30 12:00:00 +0000

    Commit with custom date: jan 30 2025 12:00:00

--- git.sh
+++ git.sh
@@ -17,4 +17,4 @@ GIT_COMMITTER_DATE="$custom_date" git commit --date="$custom_date" -m "Commit wi
 # Push the changes to the repository
 git push
 
-echo "Changes have been committed and pushed with the custom date: $custom_date"
+echo "Changes have been committed and pushed with the custom date: $custom_date successfully."
